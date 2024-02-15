10887dd9a783e65374adc56ac33fa318163da0f9
DenisPALD <masgutovmihail@gmail.com>
2024-02-15 23:24:51 +0300

    [unit_test_example] Delete unused files in unit_test_example subproject

--- src/unit_test_example/src/io_array.cpp
+++
@@ -1,8 +0,0 @@
-#include "io_array.hpp"
-
-#include <iostream>
-
-void IOArray::print_array(int arr[], const int &n) {
-    for (int i = 0; i < n; i++)
-        std::cout << arr[i] << " ";
-}
--- src/unit_test_example/src/io_array.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace IOArray {
-    void print_array(int arr[], const int &n);
-
-}
